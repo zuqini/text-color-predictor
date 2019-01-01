a66788d82d9542413d516a96920f12db3bd960fc
zuqini <lizuqiliang@gmail.com>
2019-01-01 14:35:03 -0500

    added testset, calculate accuracy, update readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Text Color Predictor
-This is a toy predictor written from scratch in Python to help improve my understanding of neural nets.
+A simple feed forward net written from scratch in Python to help me improve my understanding of neural nets and explore the effects of tuning various parameters.
 
-Given a backgound color, predicts whether a white or black text should be displayed.
+Given a backgound color - predicts whether a black or white text should be displayed for best readability. 
+
+You can quickly create your own data set using this [tool](tools/data-sample-generator/data_sample_generator.html). Drop it in `data/` and update the trainer to read from your data instead.
+## Run
+
+```
+$ python3 train.py
+```
--- train.py
+++ train.py
@@ -41,7 +41,7 @@ class FeedForwardNet:
 
     def back_prop(self):
         cost = np.sum(np.square(self.a2 - self.y))
-        if abs(cost - self.cost) > 0.001:
+        if abs(cost - self.cost) > 0.01:
             logging.info('cost: ' + str(cost))
         self.cost = cost
         delta_b2 = 2 * (self.y - self.a2) * sigmoid_derivative(self.a2)
@@ -65,16 +65,33 @@ class FeedForwardNet:
         logging.debug(delta_w1)
         logging.debug(delta_b1)
 
+    def evaluate(self, X, Y):
+        z1 = np.dot(X, self.w1) + self.b1;
+        a1 = sigmoid(self.z1)
+        z2 = np.dot(self.a1, self.w2) + self.b2;
+        a2 = sigmoid(self.z2)
+
+        result = [1 if (prediction == actual).all() else 0 for prediction, actual in zip(np.round(a2), Y)]
+        logging.info('result:')
+        logging.info(result)
+        logging.info('accuracy: ' + str(np.sum(result)/len(Y)))
+
+def gen_input_data(data):
+    return list(map(lambda data_point: list(map(lambda rgb: rgb / 255, data_point['backgroundColor'])), data))
+
+def gen_output_data(data):
+    return list(map(lambda x: x['textColor'], data))
+
 if __name__ == '__main__':
     with open('data/training-set-v1.json') as f:
-        data = json.load(f)
-    backgroundColors = list(map(lambda data_point: list(map(lambda rgb: rgb / 255, data_point['backgroundColor'])), data))
-    textColors = list(map(lambda x: x['textColor'], data))
+        training_set = json.load(f)
+    with open('data/test-set-v1.json') as f:
+        test_set = json.load(f)
 
     epoch = 5000
     learning_rate = 0.15
-    X = np.array(backgroundColors)
-    Y = np.array(textColors)
+    X = np.array(gen_input_data(training_set))
+    Y = np.array(gen_output_data(training_set))
     logging.debug(X.shape)
     logging.debug(Y.shape)
 
@@ -82,3 +99,5 @@ if __name__ == '__main__':
     for i in range(epoch):
         nn.feed_forward()
         nn.back_prop()
+
+    nn.evaluate(gen_input_data(test_set), gen_output_data(test_set))
